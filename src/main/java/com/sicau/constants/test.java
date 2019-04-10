cc4f07edc51bb4559d916f8da0f34f77ca791942
shm <1449627817@qq.com>
2019-04-10 18:28:34 +0800

    test3

---
+++ src/main/java/com/sicau/constants/test.java
@@ -0,0 +1,15 @@
+package com.sicau.constants;
+
+public class test {
+    private static String say ="我要学会你";
+    public static void main(String[] args) {
+        // TODO 自动生成的方法存根
+        System.out.println("你好 Java"+say);
+        int a=1;
+        int b=2;
+        int c=a+++b;
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
